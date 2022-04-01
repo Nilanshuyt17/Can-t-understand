f2590a05ff4b4de90d0cf4e41d93deb56ec53032
Nilanshuyt17 <91885068+Nilanshuyt17@users.noreply.github.com>
2022-04-01 11:31:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var quick_draw_data_set = aircraft carrier, airplane, alarm clock, ambulance, angel, animal migration, ant, anvil;
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.position(537, 320);
+    background("white");
+}
+// Can't understand after this
+function updateCanvas() {
+    background("white");
+    random_number = Math.floor(quick_draw_data_set);
+}
